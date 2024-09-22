fb65f465718bf192cdd0b7298f68a7be1baf2082
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-09-21 20:37:36 -0400

    Changed Student model to default for auth

--- layuplist/models.py
+++ layuplist/models.py
@@ -5,17 +5,16 @@ from django.db import models
 
 
 class Student(AbstractUser):
-    """Custom User model for students"""
+    """Custom user model for students"""
 
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.username + " " + self.email
+    email = models.EmailField("email_address", unique=True)
+    graduation_year = models.IntegerField(null=True, blank=True)
 
 
 # Create your models here.
 class Department(models.Model):
+    """Model for a department"""
+
     short_name = models.CharField(max_length=4)
     long_name = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
